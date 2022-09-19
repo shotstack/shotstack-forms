c35fadca79896aa552a400bdf139f613ba338378
Joe <jb.barrientos@yahoo.com>
2022-09-19 20:54:00 -0300

    Feat: required boilerplate for github pages/ switched adapter from auto to static site generator

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd .svelte-kit
+
+git init
+git checkout -b merge-fields-form
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:r-argentina-programa/shotstack-merge-fields.git merge-fields-form:gh-pages
+
+cd -
