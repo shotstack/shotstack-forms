37bda2a7178d72b61429dce46403a5be54da226b
Joe <jb.barrientos@yahoo.com>
2022-09-19 22:36:09 -0300

    Feat: basePath changes according to production environment instead of development due to NODE_ENV not being available during development mode

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
-const dev = process.env.NODE_ENV === 'development';
+
+const basePath = process.env.NODE_ENV === 'production' ? '/shotstack-merge-fields' : ""
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -21,7 +23,7 @@ const config = {
 		}),
 		appDir: "app",
 		paths: {
-			base: dev ? '' : '/shotstack-merge-fields'
+			base: basePath
 		},
 		trailingSlash: 'always',
 
