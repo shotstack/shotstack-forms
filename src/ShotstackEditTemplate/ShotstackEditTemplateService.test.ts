6cab66c47181e4686ef391b5ac8e4593aaa7fea7
Joe <jb.barrientos@yahoo.com>
2022-09-21 20:02:14 -0300

    Feat: adding tests for json typecasting method

--- src/ShotstackEditTemplate/ShotstackEditTemplateService.test.ts
+++ src/ShotstackEditTemplate/ShotstackEditTemplateService.test.ts
@@ -30,15 +30,51 @@ describe('ShotstackEditTemplateService.setTemplateSource', () => {
 		editTemplateService.setTemplateSource('{ "merge": [ { "find": "test", "replace": "foo" } ] }');
 
 		expect(editTemplateService.template.merge).toEqual([{ find: 'test', replace: 'foo' }]);
+
+
 	});
 
 	test('Throws error if not merge array passed', () => {
 		const editTemplateService = new ShotstackEditTemplateService();
-
+		expect(() => editTemplateService.setTemplateSource("<>")).toThrowError(INVALID_JSON)
 		expect(() => editTemplateService.setTemplateSource('{}')).toThrowError(MERGE_NOT_FOUND);
 	});
 });
 
+describe("ShotstackEditTemplateService.castIntoType", () => {
+	test('Correctly casts an element into a JSON valid type, or else returns a string', () => {
+		let service = new ShotstackEditTemplateService()
+
+		let number = service.castIntoType('1234')
+		expect(number).toEqual(1234)
+
+		let bFalse = service.castIntoType('false')
+		expect(bFalse).toEqual(false)
+
+		let bTrue = service.castIntoType('true')
+		expect(bTrue).toEqual(true)
+
+		let bNull = service.castIntoType('null')
+		expect(bNull).toEqual(null)
+
+		let string = service.castIntoType("A normal string")
+		expect(string).toEqual("A normal string")
+
+		let map = service.castIntoType(JSON.stringify({ foo: "bar" }))
+		expect(map).toEqual({ foo: "bar" })
+
+		let array = service.castIntoType(JSON.stringify(["1", 2, 3]))
+		expect(array).toEqual(["1", 2, 3])
+	})
+	test("Every invalid JSON type should return a string", () => {
+		let service = new ShotstackEditTemplateService()
+		let fn = service.castIntoType("function(){}")
+		expect(fn).toEqual("function(){}")
+
+		let und = service.castIntoType('undefined')
+		expect(und).toEqual('undefined')
+	})
+})
 describe('ShotstackEditTemplateService.updateResultMergeFields', () => {
 	test('Correctly updates the resulting merge array', () => {
 		const editTemplateService = new ShotstackEditTemplateService({
