fbb822eb40317fc91211345a35e934097c5362b0
Tomas Paez <paeztms@gmail.com>
2022-09-21 12:00:22 -0300

    test: every section of the component is rendered as espected at first load

--- cypress/e2e/exists.cy.ts
+++ cypress/e2e/exists.cy.ts
@@ -1,10 +1,22 @@
 /// <reference types="Cypress"/>
 
-describe("Smoke test", () => {
-    it("Should show the page", () => {
-        cy.visit('localhost:5173')
-        cy.get("section>h1").should("have.text", "Result")
-    })
-})
+const formContainer = '[data-cy=form-container]';
+const templateInputSection = '[data-cy=template-input-section]';
+const templateInputError = '[data-cy=template-input-error]';
+const mergeFieldsInputSection = '[data-cy=merge-fields-input-section]';
+const mergeFieldsInputError = '[data-cy=merge-fields-input-error]';
+const resultSection = '[data-cy=result-section]';
 
-export {}
+describe('Form component', () => {
+	it('Renders properly', () => {
+		cy.visit('localhost:5173');
+		cy.get(formContainer).should('be.visible');
+		cy.get(templateInputSection).should('be.visible');
+		cy.get(templateInputError).should('not.exist');
+		cy.get(mergeFieldsInputSection).should('be.visible');
+		cy.get(mergeFieldsInputError).should('not.exist');
+		cy.get(resultSection).should('be.visible');
+	});
+});
+
+export {};
