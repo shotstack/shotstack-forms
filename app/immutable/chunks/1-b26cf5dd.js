7c10ad3d36190ca811688dc3d80a9f80f29d1ec3
ClassicalJo <jb.barrientos@yahoo.com>
2022-09-21 00:58:26 +0000

    Deploying to gh-pages from @ r-argentina-programa/shotstack-merge-fields@d2236fc9b592172125e286827d9c70eaf3c3fd3c 🚀

---
+++ app/immutable/chunks/1-b26cf5dd.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-2b926f63.js";export{t as component};
---
+++ app/immutable/chunks/2-029270b7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-ae6d2e18.js";export{t as component};
