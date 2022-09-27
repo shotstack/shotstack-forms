2ed83e50265fa956e9909ea30217a9fd9a1000ca
Horacaz <xistir97@gmail.com>
2022-09-27 00:00:06 -0300

    adds and arranges tests for validateMerge

--- src/ShotstackEditTemplate/validate.test.ts
+++ src/ShotstackEditTemplate/validate.test.ts
@@ -1,50 +1,59 @@
 import defaultJSON from '../routes/default.json'
 import { FIND_NOT_EMPTY, FIND_NOT_FOUND, FIND_NOT_STRING, INVALID_JSON, MERGE_NOT_ARRAY, MERGE_NOT_EMPTY, MERGE_NOT_FOUND, REPLACE_NOT_FOUND } from './constants'
-import { validateTemplate, ValidationError } from './validate'
+import { validateTemplate, ValidationError, validateMerge } from './validate'
 describe("ShotstackEditTemplate/validate.ts", () => {
     test("If valid stringified json is passed, it should return a valid json", () => {
-        let validStringified = JSON.stringify(defaultJSON)
-        let result = validateTemplate(validStringified)
+        const validStringified = JSON.stringify(defaultJSON)
+        const result = validateTemplate(validStringified)
         expect(result).toEqual(defaultJSON)
     })
     test("Should throw an error if input is an invalid json", () => {
-        let invalidJson = "<invalid>"
+        const invalidJson = "<invalid>"
         expect(() => validateTemplate(invalidJson)).toThrowError(INVALID_JSON)
     })
 
     test("Should throw if input does not have a merge property, ", () => {
-        let noMerge = JSON.stringify({ foo: "bar" })
+        const noMerge = JSON.stringify({ foo: "bar" })
         expect(() => validateTemplate(noMerge)).toThrowError(MERGE_NOT_FOUND)
     })
 
     test("Should throw if merge is not an array", () => {
-        let noArray = JSON.stringify({ merge: { foo: "bar" } })
+        const noArray = JSON.stringify({ merge: { foo: "bar" } })
         expect(() => validateTemplate(noArray)).toThrowError(MERGE_NOT_ARRAY)
     })
 
     test("Should throw if merge is empty", () => {
-        let emptyArray = JSON.stringify({ merge: [] })
+        const emptyArray = JSON.stringify({ merge: [] })
         expect(() => validateTemplate(emptyArray)).toThrowError(MERGE_NOT_EMPTY)
-    })
-
-    test("Should throw if a merge item doesn't have the find prop", () => {
-        let noFind = JSON.stringify({ merge: [{ find: "foo", replace: "bar" }, {}] })
-        expect(() => validateTemplate(noFind)).toThrowError(FIND_NOT_FOUND)
-    })
-
-    test("Should throw if a merge item doesn't have the replace prop", () => {
-        let noFind = JSON.stringify({ merge: [{ find: "foo" }] })
-        expect(() => validateTemplate(noFind)).toThrowError(REPLACE_NOT_FOUND)
-    })
-
-    test("Should throw if find prop is not a string", () => {
-        let notString = JSON.stringify({ merge: [{ find: 123, replace: "bar" }] })
-        expect(() => validateTemplate(notString)).toThrowError(FIND_NOT_STRING)
-    })
-
-    test("Should throw if find prop is an empty string", () => {
-        let notString = JSON.stringify({ merge: [{ find: "", replace: "bar" }] })
-        expect(() => validateTemplate(notString)).toThrowError(FIND_NOT_EMPTY)
     })
 })
 
+describe("ShotstackEditTemplate/validate.ts .validateMerge", () =>{
+    test("Should throw if a merge item doesn't have the find prop", () =>{
+        const mergeNotFound = [{foo: "bar", replace: "bar"}]
+        expect( () => validateMerge(mergeNotFound)).toThrowError(FIND_NOT_FOUND)
+    })
+    test("Should throw if a merge item doesn't have the replace prop", () =>{
+        const replaceNotFound = [{find: "foo", bar: "bar"}]
+        expect( () => validateMerge(replaceNotFound)).toThrowError(REPLACE_NOT_FOUND)
+    })
+    test("Should throw if find prop is not a string", () =>{
+        const findNotString = [{find: 1, replace: "bar"}]
+        expect( () => validateMerge(findNotString)).toThrowError(FIND_NOT_STRING)
+    })
+    test("Should throw if find prop is an empty string", () =>{
+        const findEmpty = [{find:"", replace:"bar"}]
+        expect(()=> validateMerge(findEmpty)).toThrowError(FIND_NOT_EMPTY)
+    })
+    test("The value of the replace prop should always be a string when it receives a non string value", () =>{
+        const replaceIsString = [{find:"bar", replace: null}]
+        const result = validateMerge(replaceIsString)
+        expect(result).toStrictEqual([{find:"bar", replace: "null"}])
+    })
+
+    test("The value of find and replace props on a merge item should always be a string",  () =>{
+        const isAlwaysString = [{find:"bar", replace: "foo"}]
+        const result = validateMerge(isAlwaysString)
+        expect(result).toStrictEqual([{find:"bar", replace: "foo"}])
+    })
+})
