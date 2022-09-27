f69aa5c6deba1a4c7101f431d1c4e63cbcc50aed
Joe <jb.barrientos@yahoo.com>
2022-09-27 16:27:19 -0300

    Feat: Working library mode config. Adding scripts to build package without touching existing workflows

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 import type { UserConfig } from 'vite';
+import { resolve } from 'path'
 
-const config: UserConfig = {
-	plugins: [sveltekit()]
-};
+function configOptions(environment: boolean): UserConfig {
+	switch (environment) {
+		case true: return {
+			plugins: [svelte()],
+			build: {
+				outDir: 'package',
+				lib: {
+					entry: resolve(__dirname, 'src/lib/index.ts'),
+					name: "Shotstack",
+					fileName: "Shotstack",
+					
+				}
+			}
+		}
+		default: return {
+			plugins: [sveltekit()]
+		}
+	}
+}
+
+const config: UserConfig = configOptions(process.env.PACKAGE === 'package')
 
 export default config;
