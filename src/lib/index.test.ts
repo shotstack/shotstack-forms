2be4ba28c406c8c4ddf5856e528a75d485884f83
Joe <jb.barrientos@yahoo.com>
2022-10-03 04:31:08 -0300

    Feat: implementing a class as the default export of the library

--- src/lib/index.test.ts
+++ src/lib/index.test.ts
@@ -3,7 +3,8 @@ import Shotstack from './index';
 describe('Testing Shotstack module entry point', () => {
 	it('Shotstack.render() should deploy the app inside target element', () => {
 		const element = document.createElement('div');
-		Shotstack.render(element);
+		const shotstackFormService = new Shotstack();
+		shotstackFormService.render(element);
 		expect(element).toMatchSnapshot();
 	});
 });
