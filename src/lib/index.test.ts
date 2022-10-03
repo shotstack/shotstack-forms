5e3cd1c7a95d59dc1841ff44ef6c0b9bc426ca53
Joe <jb.barrientos@yahoo.com>
2022-10-03 06:13:35 -0300

    Feat: testing initialization when an element with id shotstack-form-field is present

--- src/lib/index.test.ts
+++ src/lib/index.test.ts
@@ -1,10 +1,19 @@
 import { describe, expect } from '@jest/globals';
 import Shotstack from './index';
+
 describe('Testing Shotstack module entry point', () => {
 	it('Shotstack.render() should deploy the app inside target element', () => {
 		const element = document.createElement('div');
 		const shotstackFormService = new Shotstack();
 		shotstackFormService.render(element);
+		expect(element).toMatchSnapshot();
+	});
+
+	it.only("On initialization, should render the form inside the an element with id 'shotstack-form-field' if any", () => {
+		const element = document.createElement('div');
+		element.id = 'shotstack-form-field';
+		document.body.append(element);
+		new Shotstack();
 		expect(element).toMatchSnapshot();
 	});
 });
