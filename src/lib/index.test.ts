c20bdbec420f9a93a574cdd4b25d1ecb7e93e1ab
Joe <jb.barrientos@yahoo.com>
2022-10-03 20:23:49 -0300

    Feat: adds tests for Shotstack.on(event, callback)

--- src/lib/index.test.ts
+++ src/lib/index.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect } from '@jest/globals';
 import Shotstack from './index';
 import defaultJsonInput from '../lib/components/Form/defaultMerge.json';
+import type { IParsedEditSchema } from './ShotstackEditTemplate/types';
 describe('Testing Shotstack module entry point', () => {
 	it('Shotstack.render() should deploy the app inside target element', () => {
 		const element = document.createElement('div');
@@ -21,5 +22,31 @@ describe('Testing Shotstack module entry point', () => {
 		const stringifiedJson = JSON.stringify(defaultJsonInput);
 		const shotstackService = new Shotstack(stringifiedJson);
 		expect(shotstackService.templateService.template).toEqual(defaultJsonInput);
+	});
+
+	it("When .on(event, fn) is called, it should add a 'fn' handler for that particular 'event'", () => {
+		const shotstackService = new Shotstack();
+		const mockChange = jest.fn();
+		const find = 'Hello';
+		const replace = 'World';
+		const newReplace = 'New World';
+		const mergeEntry = { find, replace };
+		const replacedEntry = { find, replace: newReplace };
+		const jsonTemplate: IParsedEditSchema = { merge: [mergeEntry] };
+		const expectedResultTemplate: IParsedEditSchema = { merge: [replacedEntry] };
+
+		shotstackService.on('change', mockChange);
+		shotstackService.templateService.setTemplateSource(JSON.stringify(jsonTemplate));
+		expect(mockChange).toHaveBeenCalledWith(jsonTemplate);
+
+		shotstackService.templateService.updateResultMergeFields(replacedEntry);
+		expect(mockChange).toHaveBeenCalledWith(expectedResultTemplate);
+		expect(mockChange).toHaveBeenCalledTimes(2);
+
+		const mockSubmit = jest.fn();
+		shotstackService.on('submit', mockSubmit);
+		shotstackService.submit();
+		expect(mockSubmit).toHaveBeenCalled();
+		expect(mockSubmit).toHaveBeenCalledWith(expectedResultTemplate);
 	});
 });
