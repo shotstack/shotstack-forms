e991af0feaf442b0ae3e5f1c18a64fb34479f6a3
Horacaz <xistir97@gmail.com>
2022-10-03 23:25:34 -0300

    Feat: Adds new methods to Shotstack class as requested

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -6,7 +6,7 @@ class Shotstack {
 	public templateService: ShotstackEditTemplateService;
 	constructor(
 		initialTemplate?: unknown,
-		public container = document.querySelector('#shotstack-form-field')
+		public containerElement = document.querySelector('#shotstack-form-field') as HTMLElement
 	) {
 		this.templateService = new ShotstackEditTemplateService(initialTemplate);
 		this.initialize();
@@ -21,7 +21,7 @@ class Shotstack {
 	}
 
 	initialize() {
-		this.container && this.render(this.container);
+		this.containerElement && this.render(this.containerElement);
 	}
 	render(container: Element) {
 		new Form({
@@ -30,6 +30,26 @@ class Shotstack {
 				editTemplateService: this.templateService
 			}
 		});
+	}
+	display() {
+		this.containerElement.style.display = 'block';
+	}
+	hide() {
+		this.containerElement.style.display = 'none';
+	}
+	attach(newElement: HTMLElement) {
+		this.remove();
+		this.containerElement = newElement;
+		this.render(this.containerElement);
+	}
+	remove() {
+		this.containerElement.remove();
+	}
+	container() {
+		return this.containerElement;
+	}
+	merge() {
+		return this.templateService.result;
 	}
 }
 
