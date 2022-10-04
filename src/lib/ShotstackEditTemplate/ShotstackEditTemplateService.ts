98bafd646540550ad68d224338a6ace9f92ba7ef
Joe <jb.barrientos@yahoo.com>
2022-10-04 01:50:36 -0300

    Refactor: replaced property with getters and getters for additional control on change event

--- src/lib/ShotstackEditTemplate/ShotstackEditTemplateService.ts
+++ src/lib/ShotstackEditTemplate/ShotstackEditTemplateService.ts
@@ -9,13 +9,20 @@ import { validateTemplate } from './validate';
 
 export class ShotstackEditTemplateService {
 	public template: IParsedEditSchema;
-	public result: IParsedEditSchema;
+	private _result: IParsedEditSchema;
 	private handlers: IShotstackHandlers;
 	constructor(template?: unknown) {
 		const parsedInitialTemplate = this.parseInitialTemplate(template);
 		this.template = parsedInitialTemplate;
-		this.result = parsedInitialTemplate;
+		this._result = parsedInitialTemplate;
 		this.handlers = { change: [], submit: [] };
+	}
+	public set result(validParsedTemplate: IParsedEditSchema) {
+		this._result = validParsedTemplate;
+		this.handlers.change.forEach((fn) => fn(validParsedTemplate));
+	}
+	public get result() {
+		return this._result;
 	}
 
 	on(eventName: TemplateEvent, callback: IShotstackEvents[TemplateEvent]) {
@@ -26,15 +33,12 @@ export class ShotstackEditTemplateService {
 		this.handlers.submit.forEach((fn) => fn(this.result));
 	}
 
-	change() {
-		this.handlers.change.forEach((fn) => fn(this.result));
-	}
-
 	parseInitialTemplate(initialTemplate: unknown) {
 		const isString = typeof initialTemplate === 'string';
 		const stringified = isString ? initialTemplate : JSON.stringify(initialTemplate);
 		try {
-			return validateTemplate(stringified);
+			const validTemplate = validateTemplate(stringified);
+			return validTemplate;
 		} catch (error) {
 			return { merge: [] };
 		}
@@ -44,7 +48,6 @@ export class ShotstackEditTemplateService {
 			const parsedTemplate = validateTemplate(jsonTemplate);
 			this.template = parsedTemplate;
 			this.result = parsedTemplate;
-			this.change();
 			return parsedTemplate;
 		} catch (err) {
 			if (err instanceof Error) throw err;
@@ -59,7 +62,6 @@ export class ShotstackEditTemplateService {
 			mergeField?.find === mergeFieldInput.find ? validMergeField : mergeField
 		);
 		this.result = { ...this.result, merge };
-		this.change();
 		return merge;
 	}
 }
