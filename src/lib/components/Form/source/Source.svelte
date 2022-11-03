d12f1b6f9bd495b62b2b559a75e7d9972506a75d
More4time <lautaromoreno1998@live.com>
2022-11-03 16:52:38 -0300

    Feat: implements getInputSource to render all src inputs

--- src/lib/components/Form/source/Source.svelte
+++ src/lib/components/Form/source/Source.svelte
@@ -11,7 +11,6 @@
 	<label class="block mb-2 monospace" for="input">Upload src</label>
 	<input class="border w-full mb-3 pl-2 py-1 text-stone-500" type="text" value={urlFile} disabled />
 
-	<label class="block mb-2 monospace" for="input-files">Add your files</label>
 	<input
 		on:change={(e) => handleChange(e.currentTarget.files)}
 		class="border w-full mb-3 pl-2 py-1 text-stone-500"
