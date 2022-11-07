920766226efe1f5d5db4de33ef8201dab61df47c
Joe <jb.barrientos@yahoo.com>
2022-11-07 08:51:52 -0300

    Adds e2e tests for source field component

--- src/lib/components/Form/source/Source.svelte
+++ src/lib/components/Form/source/Source.svelte
@@ -16,9 +16,17 @@
 
 <div data-cy="source-input">
 	<label class="block mb-2 monospace" for="input">{label.slice(2, -2).trim()}</label>
-	<input class="border w-full mb-3 pl-2 py-1 text-stone-500" type="text" {value} disabled />
+	<input
+		aria-label="Current source value"
+		class="border w-full mb-3 pl-2 py-1 text-stone-500"
+		type="text"
+		{value}
+		disabled
+	/>
 	<input
 		{disabled}
+		role="button"
+		aria-label="File upload"
 		on:change={(e) => loadFile(e.currentTarget.files)}
 		class="border w-full mb-3 pl-2 py-1 text-stone-500"
 		type="file"
